fed5bb550a5fd94a5600b9b1bd578e9b3fd4f44d
Naoya Otsuka <otknoy@gmail.com>
2021-07-05 20:46:58 +0900

    temp

--- my_server/wsgidav/start_wsgidav.sh
+++ my_server/wsgidav/start_wsgidav.sh
@@ -3,4 +3,6 @@
 useradd -m ${USER}
 echo "${USER}:${PASSWORD}" | chpasswd
 
-wsgidav --host 0.0.0.0 --port 8080 --root /var/wsgidav-root --auth=pam-login --no-config
+
+wsgidav
+# wsgidav --host 0.0.0.0 --port 8080 --root /var/wsgidav-root --auth=pam-login #-no-config
