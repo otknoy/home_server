1a0a5eb3d66483cc39f6b87d752a7aa26eccb7b2
Naoya Otsuka <otknoy@gmail.com>
2023-07-01 00:53:01 +0900

    refactor alertmanager

--- monitoring/alertmanager/docker-entrypoint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/ash
-cat /etc/alertmanager/alertmanager.template.yml | sed -e "s@\${SLACK_API_URL}@${SLACK_API_URL}@g" > /etc/alertmanager/alertmanager.yml
-
-/bin/alertmanager --config.file=/etc/alertmanager/alertmanager.yml --storage.path=/alertmanager
